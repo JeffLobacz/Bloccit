18115b08a32713454b842d6c8eff90df35cdb54b
Jeff <jeff.lobacz@gmail.com>
2015-07-02 00:51:00 -0700

    first commit for advertisements

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   
+  # get 'advertisements/index'
+  resources :advertisements
+
+  get 'advertisements/show'
+
   resources :posts
 
   get 'about' => 'welcome#about'
