20872d159d689fef3dd14b2fbff36daa5415b1df
Jeff <jeff.lobacz@gmail.com>
2015-07-10 02:11:31 -0700

    Partially completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # The following 5 lines satisfies the 3rd and 4th bullet points in the assignment:
+  # "Move the flash_attack filter to ApplicationController"
+
+  before_action :flash_attack
+
+  def flash_attack
+    flash[:notice] = "Assignment All Views Flash Attack!"
+  end
+
   protected
 
   def configure_permitted_parameters
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,25 @@
 class PostsController < ApplicationController
+  # The following 5 lines satisfies the first two bullet points in the assignment:
+  # "Add a before_action filter, called flash_attack, that sets a flash before every
+  # action in PostsController"
+
+  # before_action :flash_attack
+
+  # def flash_attack
+  #   flash[:notice] = "Assignment Post-Related Views Only Flash Attack!"
+  # end
+
+  # The following 7 lines is the beginning of an attempt to satsify the last part of the assignment --
+  # and is incomplete.  I am not sure how to go about using a "skip_before_action".
+
+  # skip_before_action :no_flash_attack
+ 
+  # def no_flash_attack
+  #   flash[:notice] = "This is not a Flash Attack!"
+  # end
+
+  # skip_before_action :flash_attack, only: [:new, :index]
+
   def index
     @posts = Post.all
   end
