bcdb9360db1c1cf2e5e0e78fb3b807740488a213
Jeff <jeff.lobacz@gmail.com>
2015-07-15 23:52:09 -0700

    scope still not working

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,22 @@
 class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
+  
+  #The two scopes that work every time:
   default_scope { order('created_at DESC') }
+  # default_scope { order('title') }
+
+  #Sanjay's suggestion:
+  #scope :ordered_by_title, -> { order(title: :asc) }
+  #produces an alphabetically random order leading me to believe they are ordered by "created_at ASC"
+
+  #Ryan's suggestion:
+  #scope :order_by_title, -> { order('title') }
+  #produces an alphabetically random order leading me to believe they are ordered by created_at ASC.
+
+  #Reed's suggestion:
+  # scope :ordered_by_title, -> { unscoped.order(title: :asc) }
+  #produces an alphabetically random order leading me to believe they are ordered by created_at ASC.
+
 end
+
