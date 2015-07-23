ff2fc3457c5851fabe20ce4107eb60f49afb33e1
Jeff <jeff.lobacz@gmail.com>
2015-07-22 20:04:48 -0700

    CP completed and working as expected

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
+  belongs_to :topic
+  
   default_scope { order('created_at DESC') }
+  # scope :order_by_title, -> { order('title') }
 end
