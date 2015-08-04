4d53d865ed93d28d8ed603f041aa2223d8f49ec1
Jeff <jeff.lobacz@gmail.com>
2015-08-04 01:54:24 -0700

    This was updated on Aug.4th at 1:54

--- db/schema.rb
+++ db/schema.rb
@@ -29,4 +29,12 @@ ActiveRecord::Schema.define(version: 20150624173948) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "questions", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.boolean  "resolved"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
