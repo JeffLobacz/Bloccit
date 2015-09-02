511067b9b0868301da62e1bfa2cbc91530188b1b
Jeff <jeff.lobacz@gmail.com>
2015-09-02 14:34:37 -0700

    Still not working after moving code to model

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,4 +10,21 @@ class Post < ActiveRecord::Base
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
   validates :user, presence: true
+
+  def markdown_title
+    render_as_markdown
+  end
+
+  def markdown_body
+    render_as_markdown
+  end
+
+  private
+
+  def render_as_markdown(markdown)
+    renderer = Redcarpet::Render::HTML.new
+    extentions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extentions)
+    (redcarpet.render markdown).html_safe
+  end
 end
