bad2244860c1bad2d5b03009d2dd0216a6fbf849
Jeff <jeff.lobacz@gmail.com>
2015-09-18 22:19:36 -0700

    Refactoring with Jose

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,9 @@ class PostsController < ApplicationController
 
   def show
     @topic = Topic.find(params[:topic_id])
-    @post = Post.find(params[:id])
+    @post = @topic.posts.find(params[:id])
     @comments = @post.comments
     authorize @post
-    
   end
 
   def new
@@ -16,9 +15,8 @@ class PostsController < ApplicationController
 
   def create
     @topic = Topic.find(params[:topic_id])
-    @post = Post.new(post_params)
+    @post = @topic.posts.build(post_params)
     @post.user = current_user
-    @post.topic = @topic
     authorize @post
 
     if @post.save
